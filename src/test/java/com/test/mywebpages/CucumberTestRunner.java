056978e08e63cf5ed98b730d38ace68a5bb17030
Sushant Wadjikar <wadjikarsushant@gmail.com>
2022-07-13 07:33:39 +0530

    Extent Report Adaptop

--- src/test/java/com/test/mywebpages/CucumberTestRunner.java
+++ src/test/java/com/test/mywebpages/CucumberTestRunner.java
@@ -1,17 +1,32 @@
 package com.test.mywebpages;
 
+import com.vimalselvam.cucumber.listener.Reporter;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 
+
+import java.io.File;
+
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/Features/login.feature","src/test/resources/Features/GoogleSearch.feature"},
-        plugin = {"pretty","json:target/cucumber/JsonReports/CucumberTest.json","html:target/cucumber/HtmlReports/HtmlReports.html"},
+        plugin = {"pretty","json:target/cucumber/JsonReports/CucumberTest.json","html:target/cucumber/HtmlReports/HtmlReports.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
         tags="@GoogleSearchTests"
 )
 public class CucumberTestRunner {
-
+   /* @AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+        Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+        Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        Reporter.setSystemInfo("Machine", 	"Windows 10" + "64 Bit");
+        Reporter.setSystemInfo("Selenium", "3.7.0");
+        Reporter.setSystemInfo("Maven", "3.5.2");
+        Reporter.setSystemInfo("Java Version", "1.8.0_151");
+    }*/
 
 }
--- src/test/java/com/test/mywebpages/MyWebTestStepdefs.java
+++ src/test/java/com/test/mywebpages/MyWebTestStepdefs.java
@@ -1,29 +1,46 @@
 package com.test.mywebpages;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+import static org.junit.Assert.assertTrue;
+
 public class MyWebTestStepdefs {
 
     WebDriver driver = null;
+    ExtentSparkReporter sparkReporter;
+    ExtentReports extent ;
+    ExtentTest test ;
 
     @Given("user is able to access the login page")
     public void verifyLoginPage() {
         // Write code here that turns the phrase above into concrete actions
 
         System.out.println("Code execution in side Given clause");
+      /*  extent = new ExtentReports();
+        sparkReporter = new ExtentSparkReporter("target/cucumber/ExtentReports/ExtentReports.html");
+        extent.attachReporter(sparkReporter);
+        test = extent.createTest("Google Search Test");
+        test.log(Status.PASS, "This is a logging event for Google Search Test, and it passed!");*/
     }
     @When("user enters the {string} and {string}")
     public void userEntersTheUsernameAndPassword(String username,String password) {
         System.out.println("Code execution in side When clause with parameters");
         System.out.println();
+
+
 
     }
     @And("user clicks on the login button")
@@ -46,6 +63,12 @@ public class MyWebTestStepdefs {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(25));
 
+        extent = new ExtentReports();
+        sparkReporter = new ExtentSparkReporter("target/cucumber/ExtentReports/ExtentReports.html");
+        extent.attachReporter(sparkReporter);
+        test = extent.createTest("Google Search Test");
+        test.log(Status.PASS, "This is a logging event for Google Search Test, and it passed!");
+
 
 
 
@@ -55,6 +78,7 @@ public class MyWebTestStepdefs {
     public void userIsOnGoogleSearchPage() {
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
+        test.log(Status.PASS, "Google page is open");
 
 
     }
@@ -62,14 +86,31 @@ public class MyWebTestStepdefs {
     @When("user enters a text in google search box")
     public void userEntersATextInGoogleSearchBox() {
         driver.findElement(By.cssSelector("input[title='Search']")).sendKeys("Barclays share price");
+        test.log(Status.PASS, "Search query in entered in text box");
     }
 
     @And("hits the enter")
     public void hitsTheEnter() {
         driver.findElement(By.cssSelector("input[value='Google Search']")).click();
+        test.log(Status.PASS, "Entered is hit");
     }
 
     @Then("many results are displayed")
     public void manyResultsAreDisplayed() {
+        // Share's high price is less than low price
+        Float highValue = Float.parseFloat(driver.findElement(By.cssSelector("div[data-attrid='High']")).getText());
+
+        Float lowValue = Float.parseFloat(driver.findElement(By.cssSelector("div[data-attrid='Low']")).getText());
+
+        assertTrue((highValue>lowValue)?true:false);
+
+        driver.close();
+
+        driver.quit();
+
+        test.log(Status.PASS, "High value is less than Low value, test is passed");
+
+        extent.flush();
+
     }
 }
